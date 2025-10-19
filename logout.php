2d10af2e48bec9b64a0fa82de96ef183ca1e643c
TheNomad11 <github@basseba.de>
2025-10-19 18:46:35 +0200

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,29 @@
+<?php
+// Start session to access session data
+session_start();
+
+// Unset all session variables
+$_SESSION = array();
+
+// Destroy the session
+session_destroy();
+
+// Delete the session cookie
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Prevent any caching of this page
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+
+// Redirect to login page
+header("Location: login.php?logout=1");
+exit();
+?>
